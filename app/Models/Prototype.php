6d9ddf01bd7468ebe8995a6af85454a8882f70e2
kcsamm <kcsamm11@gmail.com>
2022-07-13 17:10:43 +0100

    Seeded data using faker and seeder files
    Now pulling data from database

--- app/Models/Prototype.php
+++ app/Models/Prototype.php
@@ -1,42 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Prototype 
+class Prototype extends Model
 {
     use HasFactory;
 
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Prototype One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Eveniet expedita sint quo qui optio! Dolore, corrupti molestias obcaecati saepe voluptas aliquid sint qui perferendis dignissimos optio pariatur quae magni blanditiis vero, ut culpa assumenda dolorum? Autem, quas distinctio?'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Prototype Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Eveniet expedita sint quo qui optio! Dolore, corrupti molestias obcaecati saepe voluptas aliquid sint qui perferendis dignissimos optio pariatur quae magni blanditiis vero, ut culpa assumenda dolorum? Autem, quas distinctio?'
-            ]
-        ];
-    }
-
-    public static function find($id)
-    {
-        // Get all prototypes
-        $prototypes = self::all();
-
-        // Loop through $prototypes unti a match is found
-        foreach($prototypes as $prototype)
-        {
-            if($prototype['id'] == $id )
-            {
-                return $prototype;
-            }
-        }
-    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// // use App\Models\Prototype;
+
+use App\Models\Prototype;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Seed users table
+        \App\Models\User::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Seed prototype table
+        DB::table('prototypes')->insert([
+            'title' => 'Microdrone',
+            'tags' => 'drone spy surveilance',
+            'company' => 'Zinox Corp',
+            'location' => 'Tel aviv, Israel',
+            'email' => 'test@example.com',
+            'website' => 'https://www.zinox.com',
+            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aperiam voluptas, ipsa culpa eum nemo accusantium natus ab cum aliquid. Ut ad consectetur aperiam praesentium id fuga nobis, numquam dignissimos rerum quasi voluptas placeat maxime, quidem deserunt repudiandae illo nesciunt.'
+        ]);
+
+        DB::table('prototypes')->insert([
+            'title' => 'AI Home Assistant',
+            'tags' => 'ai robotics',
+            'company' => 'Nova Technologies',
+            'location' => 'Palo Alto, Ca',
+            'email' => 'mova@example.com',
+            'website' => 'https://www.novatech.com',
+            'description' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aperiam voluptas, ipsa culpa eum nemo accusantium natus ab cum aliquid. Ut ad consectetur aperiam praesentium id fuga nobis, numquam dignissimos rerum quasi voluptas placeat maxime, quidem deserunt repudiandae illo nesciunt.'
+        ]);
     }
 }
