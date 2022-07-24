6618859de8510dacc2a6f5ca3591a88a4e4d186b
kcsamm <kcsamm11@gmail.com>
2022-07-25 00:31:28 +0100

    Implemented flash message clear after few seconds

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,10 @@
 @if(session()->has('message'))
-    <div class="fixed top-0 transform left-1/2 -translate-x-1/2 bg- text-white px-48 py-3 bg-[#3E5E6D]"">
+    <div 
+        x-data="{ show: true }" 
+        x-init="setTimeout(() => show = false, 3000)" 
+        x-show="show" 
+        class="fixed top-0 transform left-1/2 -translate-x-1/2 bg- text-white px-48 py-1 bg-[#3E5E6D]"
+    >
         <p>
             {{ session('message') }}
         </p>
