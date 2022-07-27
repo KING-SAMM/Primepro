086d3b25349283bdabf964bfed161c7032de4d76
kcsamm <kcsamm11@gmail.com>
2022-07-27 16:06:18 +0100

    Included foreignId pointing to user id on prototypes migration table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // // use App\Models\Prototype;
 
+use App\Models\User;
 use App\Models\Prototype;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -18,10 +19,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Seed users table
-        \App\Models\User::factory(5)->create();
+        // Seed users table - generate five random users
+        // \App\Models\User::factory(5)->create();
 
-        // Seed prototype table
-        Prototype::factory(10)->create();
+        // Create single user with predetermined name and email
+        $user = User::factory()->create([
+            'name' => 'Phill Davids',
+            'email' => 'phil@gmail.com'
+        ]);
+
+        // Seed prototype table - assign all prototypes to the above single user
+        Prototype::factory(5)->create([
+            'user_id' => $user->id
+        ]);
     }
 }
