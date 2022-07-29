e4863cc735e41ec407b469c8658d6eda5305b6eb
kcsamm <kcsamm11@gmail.com>
2022-07-29 18:46:54 +0100

    Installed and configured tailwindcss
    public/css/app.css /nicepage.css /home.css was deleted in the process

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <title>Prototype Listings</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="/css/app.css" media="screen">
+    <link rel="stylesheet" href="/css/nicepage.css" media="screen">
     <link rel="stylesheet" href="/css/home.css" media="screen">
     <script src="//unpkg.com/alpinejs" defer></script>
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
